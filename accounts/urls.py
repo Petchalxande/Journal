8153cd4c09a5cd60330f40fee587b00b0d0ec93c
Alexander Petricca <petchalxande@gmail.com>
2022-08-29 16:31:06 +0100

    Minor code formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,13 +6,10 @@ from .views import UserProfileView, CustomPasswordChangeView, PasswordChangeDone
 urlpatterns = [
 
     path('user/', UserProfileView.as_view(), name='user'),
-    path('update-profile/<str:pk>/',
-         UpdateUserProfileView.as_view(), name='update-profile'),
+    path('update-profile/<str:pk>/',UpdateUserProfileView.as_view(), name='update-profile'),
 
     # Password and Account Management
-    path('password/change-done/', PasswordChangeDoneView.as_view(),
-         name='account_change_password_done'),
-    path('password/change/', CustomPasswordChangeView.as_view(),
-         name='account_change_password'),
+    path('password/change-done/', PasswordChangeDoneView.as_view(),name='account_change_password_done'),
+    path('password/change/', CustomPasswordChangeView.as_view(),name='account_change_password'),
     path('', include('allauth.urls')),
 ]
